d42be99bf28d1b030f0fda213d1a083338179584
benny <362448070@qq.com>
2019-07-06 19:16:43 +0800

    ajax登陆校验完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,5 +1,6 @@
 $(function(){
-    $("form").bootstrapValidator({
+    // 添加验证
+    $("#form").bootstrapValidator({
         // 配置校验图标
         feedbackIcons: {
             valid: 'glyphicon glyphicon-ok',
@@ -13,10 +14,13 @@ $(function(){
                     notEmpty: {
                         message:"用户名不能为空",
                         },
-                    stringLength:{
+                    stringLength: {
                         min: 2,
                         max: 6,
                         message: "用户名长度必须在2-6位"
+                    },
+                    callback: {
+                        message: "用户名不存在"
                     }
                 }
             },
@@ -29,10 +33,44 @@ $(function(){
                         min: 6,
                         max: 12,
                         message: "密码长度必须6-12位"
+                    },
+                    callback: {
+                        message: "密码错误"
                     }
                 }
 
             }
         }
-    })
+    });
+
+
+    //重置验证状态
+    $('[type="reset"]').click(function(){
+        $('#form').data('bootstrapValidator').resetForm()
+    });
+
+
+    //发送ajax请求
+    $("#form").on('success.form.bv', function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "post",
+            url: "/employee/employeeLogin",
+            dataType: "json",
+            data: $("#form").serialize(),
+            success: function( info ){
+                console.log( info );
+                if(info.success){
+                    location.href=("index.html")
+                }
+                if(info.error===1000){
+                    $('#form').data('bootstrapValidator').updateStatus('username','INVALID','callback')
+                }
+                if(info.error===1001){
+                    $('#form').data('bootstrapValidator').updateStatus('password','INVALID','callback')
+                }
+        }
+        })
+    });
+
 });
